6ba769fcc9835ca6dee0854dbf4d894b460ee61c
Nicole <nicolesyling@gmail.com>
2023-07-06 06:00:29 -0700

    Add accordion styling to FAQ page

---
+++ js/accordion.js
@@ -0,0 +1,14 @@
+let accordion = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
